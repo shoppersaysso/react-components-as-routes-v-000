d20441ed55c9a9beba4a3d72bd88a073d0d79599
Sara Hopper <github email address>
2017-11-07 13:31:33 -0500

    done

--- src/index.js
+++ src/index.js
@@ -1,14 +1,107 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
+
+const link = {
+  width: '100px',
+  padding: '12px',
+  margin: '0 6px 6px',
+  background: 'blue',
+  textDecoration: 'none',
+  color: 'white',
+}
+
+const NavBar = () =>
+  <div>
+    <NavLink
+      to="/"
+      exact
+      style={link}
+      activeStyle={{
+        background: 'darkBlue'
+      }}
+    >About</NavLink>
+    <NavLink
+      to="/login"
+      exact
+      style={link}
+      activeStyle={{
+        background: 'darkBlue'
+      }}
+    >Login</NavLink>
+    <NavLink
+      to="/signup"
+      exact
+      style={link}
+      activeStyle={{
+        background: 'darkBlue'
+      }}
+    >Sign Up</NavLink>
+    <NavLink
+      to="/messages"
+      exact
+      style={link}
+      activeStyle={{
+        background: 'darkBlue'
+      }}
+    >Messages</NavLink>
+  </div>
+
+const Home = () => <h1>Home</h1>;
+
+const About = () => <h1>About</h1>;
+
+const Login = () =>
+  <div>
+    <h1>Login</h1>
+    <form>
+      <div>
+        <input type="text" name="username" placeholder="Username" />
+        <label htmlFor="username">Username</label>
+      </div>
+      <div>
+        <input type="text" name="password" placeholder="Password" />
+        <label htmlFor="password">Password</label>
+      </div>
+        <input type="submit" value="Login" />
+    </form>
+  </div>;
+
+const SignUp = () =>
+  <div>
+    <h1>Sign Up</h1>
+    <form>
+      <div>
+        <input type="text" name="username" placeholder="Username" />
+        <label htmlFor="username">Username</label>
+      </div>
+      <div>
+        <input type="text" name="password" placeholder="Password" />
+        <label htmlFor="password">Password</label>
+      </div>
+        <input type="submit" value="Sign Up" />
+    </form>
+  </div>;
+
+const Messages = () => <h1>Messages</h1>;
 
 const App = () => {
   return (
-    <div>
-    </div>
+    <Router>
+      <div>
+        <NavBar />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/signup" component={SignUp} />
+        <Route exact path="/messages" component={Messages} />
+
+      </div>
+    </Router>
   );
 };
 
 ReactDOM.render(
-  <App />, 
+  <App />,
   document.getElementById('root')
 );
